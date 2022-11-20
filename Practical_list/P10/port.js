ba454ae5798abec32b66c694e7139f4ef42c46e9
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-11-20 23:41:18 +0530

    Formatting changes
    Signed-off-by: Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>

--- Practical_list/P10/port.js
+++ Practical_list/P10/port.js
@@ -3,11 +3,11 @@ const hostname = "localhost";
 const port = 10000;
 
 const server = http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader("Content-Type", "text/html");
- res.end("<html><head><h2>Hello World</h2></head></html>");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><head><h2>Hello World</h2></head></html>");
 });
 
 server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
